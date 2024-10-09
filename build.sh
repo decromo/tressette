891630801e0ce2e05f978c73aefe5194e4ab27bd
Diego Esteban Concepción <git@dieg.one>
2024-10-09 22:59:42 +0200

    whoops

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc utils.c network.c common.c threads.c client.c client_network.c -pthreads -g -o client;
-gcc utils.c network.c common.c threads.c server.c server_network.c -pthreads -g -o server;
+gcc utils.c network.c common.c thread.c client.c client_network.c -pthreads -g -o client;
+gcc utils.c network.c common.c thread.c server.c server_network.c -pthreads -g -o server;
