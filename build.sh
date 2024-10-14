a9c4b7edd18db4e9ca09951bb29cc0e727325291
Diego Esteban Concepción <git@dieg.one>
2024-10-14 21:14:53 +0200

    flake and build script done

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-gcc utils.c network.c common.c threads.c client.c client_network.c -Wall -pthread -g -o client;
-gcc utils.c network.c common.c threads.c server.c server_network.c -Wall -pthread -g -o server;
+if [[ -z $DEST ]]
+    then DEST="$(pwd)"
+fi
+
+if [[ -z $SOURCE ]]
+    then SOURCE="src"
+fi
+
+cd $SOURCE
+
+gcc utils.c network.c common.c threads.c client.c client_network.c -Wall -pthread -g -o $DEST/client;
+gcc utils.c network.c common.c threads.c server.c server_network.c -Wall -pthread -g -o $DEST/server;
--- flake.nix
+++ flake.nix
@@ -8,19 +8,34 @@
   outputs = { self, nixpkgs }: 
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs;
+    pkgs = import nixpkgs { inherit system; };
+    mkBin = name: pkgs.runCommandCC name { src = ./.; } ''
+      mkdir -p $out/bin
+      pushd .
+      cd $src
+      DEST=/build $src/build.sh && popd; cp -t $out/bin ${name}
+    '';
   in {
-    packages.${system} = {
-      raylib-shared = 
-      raylib-static = 
-      raylib-web = 
-      default = pkgs.hello;
+    packages.${system} = rec {
+      raylib-shared = pkgs.callPackage ./nix/raylib.nix { sharedLib = true; };
+      raylib-static = pkgs.callPackage ./nix/raylib.nix { sharedLib = false; };
+      raylib-web = pkgs.callPackage ./nix/raylib.nix { webPlatform = true; };
+      raylib-nixpkgs = pkgs.raylib;
+
+      client = mkBin "client";
+
+      server = mkBin "server";
+
+      all = mkBin "client server";
+
+      default = client;
     };
 
     devShells.${system}.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [ 
-        raylib
-      ] ++ (builtins.attrValues self.packages.${system});
+      nativeBuildInputs = with self.packages.${system}; [ 
+        raylib-static
+        client
+      ];
     };
 
   };
